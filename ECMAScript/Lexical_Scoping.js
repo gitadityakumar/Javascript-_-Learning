9f6ab9b3d7102853c207be9e652675466d945007
Aditya kumar <iamadityakumar05@proton.me>
2023-07-26 23:48:53 +0530

    Add files via upload
    covered lexical scoping

---
+++ ECMAScript/Lexical_Scoping.js
@@ -0,0 +1,11 @@
+function init(){
+    let name = "Brave";
+    function displayName(){
+        console.log(name);
+    }
+    displayName();
+}
+init();
+
+// learn more from docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures#lexical_scoping
+// https://www.youtube.com/watch?v=VaH09NXQZ58&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=49
