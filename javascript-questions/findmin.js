39deee0bf7b01770c4d68fe58f193e3b46ca9d73
gitadityakumar <itsadityasingh05@gmail.com>
2024-03-08 23:06:24 +0530

    sol

---
+++ javascript-questions/findmin.js
@@ -0,0 +1,11 @@
+ let num1 = 19;
+ let num2 = 34;
+ let num3 = 343;
+
+function findmin(num1,num2,num3){
+	return Math.min(num1,num2,num3);
+
+}
+
+let ans = findmin(num1,num2, num3);
+console.log("Minimun number is : " + ans);
