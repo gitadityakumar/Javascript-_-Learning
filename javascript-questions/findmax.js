3aa823d5c830fa88afa455aca2cc6a35c5d65807
gitadityakumar <itsadityasingh05@gmail.com>
2024-03-08 22:46:18 +0530

    solveing js questions

---
+++ javascript-questions/findmax.js
@@ -0,0 +1,12 @@
+function findmax(num1,num2,num3){
+	return Math.max(num1,num2,num3);
+	
+}
+
+let num1 = 10;
+let num2 = 28;
+let num3 = 29;
+
+let maximum = findmax(num1,num2,num3);
+
+console.log("maximum number is : " + maximum);
