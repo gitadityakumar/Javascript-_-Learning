c59afc7f29c9e408bff2c39b6ece73edac432184
gitadityakumar <itsadityasingh05@gmail.com>
2024-03-10 00:18:25 +0530

    dyanmic js , not gonna solve que here

---
+++ javascript-questions/even_odd.js
@@ -0,0 +1,13 @@
+//define a program to find out whether a funciton is even or odd
+//
+
+let ans = evenOdd(10);
+console.log(ans);
+
+function evenOdd(num){
+         if(num%2==0){
+		 return "even";
+	 }else{
+		 return "odd";
+	 }
+}
