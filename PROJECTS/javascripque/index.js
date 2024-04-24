cc903ab604f561887fb47cbdaef0a4db4ccb5ca7
gitadityakumar <itsadityasingh05@gmail.com>
2024-04-24 22:51:00 +0530

    steaky-notes

---
+++ PROJECTS/javascripque/index.js
@@ -0,0 +1,6 @@
+ var rect = document.querySelector("#center");
+
+ rect.addEventListener("mousemove",function(details){
+    var rectanglelocation = (rect.getBoundingClientRect());
+    console.log(details.clientX - rectanglelocation.left);
+ })
